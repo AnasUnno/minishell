e7f23f94eb46111e909241233cf6a8369479cfb5
Anas Raji Afoua <araji-af@e3r11p1.1337.ma>
2023-10-17 14:55:05 +0100

    final maybe :)

---
+++ builts/cd_cmd.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_cmd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: araji-af <araji-af@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:56:24 by araji-af          #+#    #+#             */
+/*   Updated: 2023/10/13 14:52:07 by araji-af         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+int	cd_old_pwd(char *path, t_data **envi)
+{
+	t_data	*tmp;
+
+	if (ft_strcmp(path, "-"))
+	{
+		ft_printf("Minishell: cd: -%s: invalid option\n", path);
+		return (1);
+	}
+	else
+	{
+		tmp = get_variable(*envi, "OLDPWD");
+		if (tmp->value)
+		{
+			if (chdir(tmp->value) == -1)
+			{
+				ft_printf("minishell: cd: %s: No such file or directory\n",
+					tmp->value);
+				return (1);
+			}
+		}
+		else
+			return (ft_printf("Minishell: cd: OLDPWD not set\n"), 1);
+	}
+	return (0);
+}
+
+int	cd_home(char *path, t_data **envi)
+{
+	t_data	*tmp;
+
+	tmp = get_variable(*envi, "HOME");
+	if (!path)
+	{
+		if (!tmp->value)
+			ft_printf("Minishell: cd: HOME not set\n");
+		else
+		{
+			if (chdir(tmp->value) == -1)
+			{
+				ft_printf("minishell: cd: %s: No such file or directory\n", 
+					tmp->value);
+				return (1);
+			}
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	cd_home2(char *path, t_data **envi)
+{
+	t_data	*tmp;
+
+	if (ft_strcmp(path, "~"))
+		ft_printf("Minishell: cd: %s: No such file or directory\n", path);
+	else
+	{
+		tmp = get_variable(*envi, "HOME");
+		if (!tmp->value)
+			ft_printf("Minishell: cd: HOME not set\n");
+		else
+		{
+			if (chdir(tmp->value) == -1)
+			{
+				ft_printf("minishell: cd: %s: No such file or directory\n",
+					tmp->value);
+				return (1);
+			}
+			return (0);
+		}
+	}
+	return (1);
+}
+
+void	update_pwd(char *path, t_data **tmp, t_data **tmp2, char *str)
+{
+	if (path[0] == '-')
+	{
+		(*tmp2)->value = (*tmp)->value;
+		(*tmp)->value = str;
+	}
+	else
+	{
+		str = (*tmp)->value;
+		if ((*tmp)->value)
+			free((*tmp)->value);
+		(*tmp)->value = getcwd(NULL, 0);
+		if ((*tmp2)->value)
+			free((*tmp2)->value);
+		(*tmp2)->value = str;
+	}
+}
+
+int	cd(char *path, t_data **envi)
+{
+	int		r_type;
+	t_data	*tmp;
+	t_data	*tmp2;
+	char	*str;
+
+	r_type = 0;
+	tmp = get_variable(*envi, "PWD");
+	tmp2 = get_variable(*envi, "OLDPWD");
+	str = tmp2->value;
+	if (!path)
+		r_type = cd_home(path, envi);
+	else if (path[0] == '-')
+		r_type = cd_old_pwd(path, envi);
+	else if (path[0] == '~')
+		r_type = cd_home2(path, envi);
+	else
+		r_type = chdir(path);
+	if (!r_type)
+		update_pwd(path, &tmp, &tmp2, str);
+	else
+		return (ft_printf("Minishell: cd: OLDPWD not set\n"), 1);
+	return (r_type);
+}
---
+++ builts/utils2.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: araji-af <araji-af@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 14:19:02 by araji-af          #+#    #+#             */
+/*   Updated: 2023/10/17 14:44:24 by araji-af         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	str = malloc(sizeof (char) * (ft_strlen(s1) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	new = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!new)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		new[i] = s2[j];
+		i++;
+		j++;
+	}
+	new[i] = '\0';
+	return (new);
+}
